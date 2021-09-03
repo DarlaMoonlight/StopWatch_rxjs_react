4a5a31c2afe17f1a2582cb5838f8f55fcdf05f05
Viktoriia Cheriasova <darla.moonlight@gmail.com>
2021-09-03 15:27:16 +0300

    correct code style

--- stopwatch_app/src/Component/Button_StopWatch.js
+++ stopwatch_app/src/Component/Button_StopWatch.js
@@ -8,14 +8,12 @@ function Button_StopWatch(props){
             <input type="submit" onClick={(e)=>{
                 setTimeout(()=>{
                     if(e.detail>=2){
-                        console.log(e.detail);
                         props.wait();
                     }
                 }, 300)
             }} value="Wait" className="btn btn-yel"/>
             <input type="submit" onClick={props.reset} value="Reset" className="btn btn-blue"/>
         </div>
-
     );
 }
 
